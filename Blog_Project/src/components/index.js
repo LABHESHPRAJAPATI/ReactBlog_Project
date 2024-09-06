2522e2fbc09fd5ed76a041282a79b3a3d9cc9b9f
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-06 22:26:29 +0530

    add authlayout , login,postcard,select , sigh up cpmponts

--- Blog_Project/src/components/index.js
+++ Blog_Project/src/components/index.js
@@ -3,5 +3,19 @@ import footer from "./footer/footer.jsx";
 import Container from "./container/container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/logoutBtn.jsx";
+import Input from "./input.jsx";
+import Button from "./Button.jsx";
+import Select from "./Select.jsx";
+import PostCard from "./PostCard.jsx";
 
-export { header, footer, Container, Logo, LogoutBtn };
+export {
+  header,
+  footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  PostCard,
+};
